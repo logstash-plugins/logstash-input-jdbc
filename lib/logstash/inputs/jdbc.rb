0dbea32c13caf970657348eabf649dfaa938c8dc
Raghav Garg <ansraghav100@gmail.com>
2016-11-22 17:03:00 +0530

    Corrected the meaning of the statement.

--- lib/logstash/inputs/jdbc.rb
+++ lib/logstash/inputs/jdbc.rb
@@ -4,7 +4,7 @@ require "logstash/namespace"
 require "logstash/plugin_mixins/jdbc"
 require "yaml" # persistence
 
-# This plugin was created as a way to ingest data in any database
+# This plugin was created as a way to ingest data from any database
 # with a JDBC interface into Logstash. You can periodically schedule ingestion
 # using a cron syntax (see `schedule` setting) or run the query one time to load
 # data into Logstash. Each row in the resultset becomes a single event.
