4366f27f55642379636c7449d95af79b8c616868
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-05-31 16:44:57 -0400

    Adjusting the build scripts to correctly load the logstash source and allow people to override it

--- ci/build.sh
+++ ci/build.sh
@@ -8,11 +8,11 @@
 set -e
 
 echo "Starting build process in: `pwd`"
-./ci/setup.sh
+source ./ci/setup.sh
 
 if [[ -f "ci/run.sh" ]]; then
     echo "Running custom build script in: `pwd`/ci/run.sh"
-    ./ci/run.sh
+    source ./ci/run.sh
 else
     echo "Running default build scripts in: `pwd`/ci/build.sh"
     bundle install
